aff9024c8216bbd66004c0ff15cb55c2fd9a148c
Asekeeewka <38551840+Asekeeewka@users.noreply.github.com>
2018-12-16 00:47:33 +0600

    TOP PAGE
    create htaccess file to proceed our top.html page correctly please.
    Write in that file "AddType application/x-httpd-php .html"

---
+++ anime.php
@@ -0,0 +1,52 @@
+<?php 
+$animelist = [
+["id"=>1,"name"=>"Fairy Tail S1","rating"=>9.15,"views"=>8714692,"votes"=>20220,"image"=>"https://yummyanime.com/img/posters/1512496716.jpg","link"=>"https://yummyanime.com/catalog/item/skazka-o-hvoste-fei","year"=>2009,"genre"=>"fantasy"],
+["id"=>2,"name"=>"Bleach","rating"=>9.09,"views"=>5389211,"votes"=>10947,"image"=>"https://yummyanime.com/img/posters/314.jpg","link"=>"https://yummyanime.com/catalog/item/blich","year"=>2004,"genre"=>"fantasy"],
+["id"=>3,"name"=>"Darling in the FranXX","rating"=>9.00,"views"=>5076412,"votes"=>27276,"image"=>"https://yummyanime.com/img/posters/1514574607.jpg","link"=>"https://yummyanime.com/catalog/item/milyj-vo-frankce","year"=>2018,"genre"=>"mecha"],
+["id"=>4,"name"=>"Sword Art Online","rating"=>9.12,"views"=>5009789,"votes"=>38135,"image"=>"https://yummyanime.com/img/posters/1514974917.jpg","link"=>"https://yummyanime.com/catalog/item/skazka-o-hvoste-fei","year"=>2012,"genre"=>"fantasy"],
+["id"=>5,"name"=>"Black Clover","rating"=>9.10,"views"=>4193163,"votes"=>6194,"image"=>"https://yummyanime.com/img/posters/1531229062.png","link"=>"https://yummyanime.com/catalog/item/mastera-mecha-onlajn","year"=>2017,"genre"=>"magic"],
+["id"=>6,"name"=>"Re:Zero","rating"=>8.88,"views"=>4013166,"votes"=>30773,"image"=>"https://yummyanime.com/img/posters/1527381687.jpg","link"=>"https://yummyanime.com/catalog/item/zhizn-v-drugom-mire-s-nulya","year"=>2016,"genre"=>"adventure"],
+["id"=>7,"name"=>"Blue Exorcist S1","rating"=>9.11,"views"=>3360749,"votes"=>22073,"image"=>"https://yummyanime.com/img/posters/292.jpg","link"=>"https://yummyanime.com/catalog/item/sinij-ekzorcist","year"=>2011,"genre"=>"fantasy"],
+["id"=>8,"name"=>"My Hero Academia S3","rating"=>9.26,"views"=>3281008,"votes"=>8073,"image"=>"https://yummyanime.com/img/posters/1519137126.jpg","link"=>"https://yummyanime.com/catalog/item/moya-gerojskaya-akademiya-3","year"=>2018,"genre"=>"school"],
+["id"=>9,"name"=>"Seven Deadly Sins S2","rating"=>9.43,"views"=>3230684,"votes"=>11095,"image"=>"https://yummyanime.com/img/posters/1518075268.png","link"=>"https://yummyanime.com/catalog/item/sem-smertnyh-grehov-2","year"=>2018,"genre"=>"action"],
+["id"=>10,"name"=>"Tokyo Ghoul S1","rating"=>8.95,"views"=>3228738,"votes"=>30972,"image"=>"https://yummyanime.com/img/posters/1519225029.jpg","link"=>"https://yummyanime.com/catalog/item/tokijskij-gul","year"=>2014,"genre"=>"drama"],
+["id"=>11,"name"=>"Attack on Titan S1","rating"=>9.24,"views"=>3211089,"votes"=>29710,"image"=>"https://yummyanime.com/img/posters/425.jpg","link"=>"https://yummyanime.com/catalog/item/vtorzhenie-gigantov","year"=>2013,"genre"=>"fantasy"],
+["id"=>12,"name"=>"KonoSuba","rating"=>9.03,"views"=>3174047,"votes"=>25174,"image"=>"https://yummyanime.com/img/posters/1541806059.jpg","link"=>"https://yummyanime.com/catalog/item/etot-zamechatel-nyj-mir","year"=>2016,"genre"=>"adventure"],
+["id"=>13,"name"=>"Demons","rating"=>8.86,"views"=>3165782,"votes"=>17949,"image"=>"https://yummyanime.com/img/posters/228.jpg","link"=>"https://yummyanime.com/catalog/item/demony-starshej-shkoly","year"=>2012,"genre"=>"school"],
+["id"=>14,"name"=>"Seven Deadly Sins S1","rating"=>9.37,"views"=>3154716,"votes"=>18241,"image"=>"https://yummyanime.com/img/posters/763.jpg","link"=>"https://yummyanime.com/catalog/item/sem-smertnyh-grehov","year"=>2014,"genre"=>"action"],
+["id"=>15,"name"=>"Fullmetal Alchemist","rating"=>9.43,"views"=>3055845,"votes"=>12615,"image"=>"https://yummyanime.com/img/posters/356.jpg","link"=>"https://yummyanime.com/catalog/item/stalnoj-alhimik-tv1","year"=>2003,"genre"=>"adventure"],
+["id"=>16,"name"=>"Fairy Tail S2","rating"=>9.35,"views"=>3049436,"votes"=>12231,"image"=>"https://yummyanime.com/img/posters/624.jpg","link"=>"https://yummyanime.com/catalog/item/skazka-o-hvoste-fei-tv-2","year"=>2014,"genre"=>"adventure"],
+["id"=>17,"name"=>"Akame ga kill","rating"=>9.43,"views"=>2991199,"votes"=>30086,"image"=>"https://yummyanime.com/img/posters/1512496716.jpg","link"=>"https://yummyanime.com/catalog/item/ubijca-akame","year"=>2014,"genre"=>"action"],
+["id"=>18,"name"=>"No game - No life","rating"=>9.47,"views"=>2918703,"votes"=>29835,"image"=>"https://yummyanime.com/img/posters/1512475460.jpg","link"=>"https://yummyanime.com/catalog/item/igra-na-vyzhivanie","year"=>2014,"genre"=>"games"],
+["id"=>19,"name"=>"Boruto: Naruto next Generations","rating"=>7.67,"views"=>2820090,"votes"=>4469,"image"=>"https://yummyanime.com/img/posters/1518075301.png","link"=>"https://yummyanime.com/catalog/item/boruto-novoe-pokolenie","year"=>2017,"genre"=>"adventure"],
+["id"=>20,"name"=>"Maid Sama!","rating"=>9.28,"views"=>2783197,"votes"=>19680,"image"=>"https://yummyanime.com/img/posters/1527381968.jpg","link"=>"https://yummyanime.com/catalog/item/prezident-gornichnaya","year"=>2010,"genre"=>"comedy"],
+["id"=>21,"name"=>"Naruto: Shippuden","rating"=>9.25,"views"=>2746552,"votes"=>11842,"image"=>"https://yummyanime.com/img/posters/877.jpg","link"=>"https://yummyanime.com/catalog/item/naruto-uragannye-hroniki","year"=>2007,"genre"=>"adventure"],
+["id"=>22,"name"=>"Death Note","rating"=>9.01,"views"=>2745339,"votes"=>9.41,"image"=>"https://yummyanime.com/img/posters/233.jpg","link"=>"https://yummyanime.com/catalog/item/tetrad-smerti","year"=>2006,"genre"=>"thriller"],
+["id"=>23,"name"=>"Sousei no Onmyouji","rating"=>9.15,"views"=>2725841,"votes"=>11976,"image"=>"https://yummyanime.com/img/posters/1381.jpg","link"=>"https://yummyanime.com/catalog/item/dve-zvezdy-onmedzhi","year"=>2016,"genre"=>"fantasy"],
+["id"=>24,"name"=>"Noragami","rating"=>9.30,"views"=>2661828,"votes"=>30093,"image"=>"https://yummyanime.com/img/posters/556.jpg","link"=>"https://yummyanime.com/catalog/item/bezdomnyj-bog-tv-1","year"=>2014,"genre"=>"fantasy"],
+["id"=>25,"name"=>"My Hero Academia S2","rating"=>9.30,"views"=>2620420,"votes"=>11776,"image"=>"https://yummyanime.com/img/posters/2175.jpg","link"=>"https://yummyanime.com/catalog/item/moya-gerojskaya-akademiya-tv-2","year"=>2017,"genre"=>"school"],
+["id"=>26,"name"=>"Sakurasou no Pet na Kanojo","rating"=>9.23,"views"=>2602519,"votes"=>20225,"image"=>"https://yummyanime.com/img/posters/523.jpg","link"=>"https://yummyanime.com/catalog/item/koshechka-iz-sakuraso","year"=>2012,"genre"=>"comedy"],
+["id"=>27,"name"=>"One Punch Man","rating"=>9.40,"views"=>2515946,"votes"=>25877,"image"=>"https://yummyanime.com/img/posters/1079.jpg","link"=>"https://yummyanime.com/catalog/item/vanpanchmen","year"=>2015,"genre"=>"action"],
+["id"=>28,"name"=>"Mirai Nikki","rating"=>8.76,"views"=>2469052,"votes"=>24842,"image"=>"https://yummyanime.com/img/posters/1531755030.jpg","link"=>"https://yummyanime.com/catalog/item/dnevnik-budushhego","year"=>2011,"genre"=>"adventure"],
+["id"=>29,"name"=>"Hunter x Hunter","rating"=>9.56,"views"=>2291355,"votes"=>5250,"image"=>"https://yummyanime.com/img/posters/868.jpg","link"=>"https://yummyanime.com/catalog/item/hanter-h-hanter","year"=>2011,"genre"=>"adventure"],
+["id"=>30,"name"=>"Kuroko no Basuke S1","rating"=>9.17,"views"=>1885688,"votes"=>12057,"image"=>"https://yummyanime.com/img/posters/330.jpg","link"=>"https://yummyanime.com/catalog/item/basketbol-kuroko","year"=>2012,"genre"=>"school"]
+];
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "anime";
+
+$db_conn = mysqli_connect($host,$username,$password,$db);
+$connection_error = mysqli_connect_error();
+if($connection_error != null){
+	echo "Error";
+}
+
+for ($i=0; $i<count($animelist); $i++) { 
+	echo $animelist[$i]["id"];
+	$query = "INSERT INTO `anime-list`(`id`, `name`, `rating`, `views`, `votes`, `image`, `link`,`year`,`genre`) VALUES ('".$animelist[$i]["id"]."','".$animelist[$i]["name"]."','".$animelist[$i]["rating"]."','".$animelist[$i]["views"]."','".$animelist[$i]["votes"]."','".$animelist[$i]["image"]."','".$animelist[$i]["link"]."','".$animelist[$i]["year"]."','".$animelist[$i]["genre"]."')";
+	$result = mysqli_query($db_conn, $query);
+}
+
+?>
